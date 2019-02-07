cd70d9e228c624f4c2d78422a52d5ba77e7a1f01
gogumassafy <gogumassafy@gmail.com>
2019-02-08 00:10:59 +0900

    c++

--- c++/Book/190127/question02-1/question02-1-3.cpp
+++ c++/Book/190127/question02-1/question02-1-3.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 
 // 교재 코드
-void SwapPointer(int *(&pref1), int *(&pref2)){ // 얘는 내 코드와 의미하는 바가 같지 않나?
-    std::cout<<"pref1: "<<pref1<<std::endl;
-    std::cout<<"&pref1: "<<&pref1<<std::endl;
-    int *ptr = pref1;
-    // std::cout<<"ptr: "<<ptr<<std::endl;
-    // std::cout<<"*ptr: "<<*ptr<<std::endl;
-    pref1 = pref2;
-    pref2 = ptr;
-}
+// void SwapPointer(int *(&pref1), int *(&pref2)){ // 얘는 내 코드와 의미하는 바가 같지 않나?
+//     // std::cout<<"pref1: "<<pref1<<std::endl;
+//     // std::cout<<"&pref1: "<<&pref1<<std::endl;
+//     int *ptr = pref1;
+//     // std::cout<<"ptr: "<<ptr<<std::endl;
+//     // std::cout<<"*ptr: "<<*ptr<<std::endl;
+//     pref1 = pref2;
+//     pref2 = ptr;
+// }
 
-// void SwapPointer(int &ptr1, int &ptr2){
-//     ptr1
+// 아래 코드의 문제점은 너무나 당연하다. 포인터를 사용하면 call by reference의 가능성을 열어둔것일뿐 반드시 call by reference는 아님을 명심하라.
+// 아래와 같이 코드를 짜면 pointer가 담고있는 주소값을 변경하기만 하는 call by value가 된다.
+// void SwapPointer(int *ptr1, int *ptr2){ 
+//     int *temp = ptr1;
+//     ptr1 = ptr2;
+//     ptr2 = temp;
 // }
 
 int main(void)
