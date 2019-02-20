40f593c7a05d93289b8ab86d7e1d6ec4b98b455e
gogumassafy <gogumassafy@gmail.com>
2019-02-20 17:55:16 +0900

    hi

---
+++ algorithm/SWexpert/프로세스_연결하기.py
@@ -0,0 +1,24 @@
+# 1767
+import sys
+sys.stdin = open("프로세스_연결하기.txt")
+
+row = [1, -1, 0, 0]
+col = [0, 0, 1, -1]
+
+def DFS(cnt):
+
+    return
+
+T = int(input())
+
+for tc in range(T):
+    N = int(input())
+    input_list = [list(map(int, input().split())) for _ in range(N)]
+    count = 0
+
+    for i in range(N):
+        for j in range(N):
+            input_list
+
+
+    print(f'#{tc+1} {}')
