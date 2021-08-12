bb2a0fbac05cb28251babef95b3b6660381d11fb
gogumassfay <gogumassafy@gmail.com>
2021-08-12 20:32:06 +0900

    디스크 컨트롤러
    Heap을 구현해서 PQ를 이용하는 풀이로 변경했다.

--- JavaScript/디스크_컨트롤러.js
+++ JavaScript/디스크_컨트롤러.js
@@ -3,29 +3,27 @@
 // Level 3
 
 function solution(jobs) {
+    debugger
     var answer = 0;
     
     jobs.sort((a, b) => {
         return a[0] - b[0];
     })
     
-    const workQue = [];
+    const workQue = new PriorityQue();
     let curTime = 0;
     const jobsCount = jobs.length;
     
-    while (jobs.length > 0 || workQue.length > 0) {
+    while (jobs.length > 0 || workQue.size() > 0) {
         
         while (jobs.length > 0 && jobs[0][0] <= curTime) {
             workQue.push(jobs.shift());
         }
         
-        workQue.sort((a, b) => {
-            return a[1] - b[1];
-        })
-        
-        if (workQue.length > 0) {
-            curTime += workQue[0][1];
-            answer += curTime - workQue.shift()[0];
+        if (workQue.size() > 0) {
+            const data = workQue.pop();
+            curTime += data[1];
+            answer += curTime - data[0];
         }
         else {
             curTime = jobs[0][0];
@@ -33,4 +31,81 @@ function solution(jobs) {
     }
     
     return parseInt(answer / jobsCount);
+}
+
+class PriorityQue {
+    #container;
+    
+    constructor() {
+        this.#container = [0];
+    }
+    
+    clear() {
+        this.#container = [0];
+    }
+    
+    _compare(a, b) {
+        return this.#container[a][1] - this.#container[b][1];
+    }
+    
+    _swap(a, b) {
+        let tmp = this.#container[a];
+        this.#container[a] = this.#container[b];
+        this.#container[b] = tmp;
+    }
+    
+    size() {
+        return this.#container.length - 1;
+    }
+    
+    top() {
+        return this.#container[1];
+    }
+    
+    push(data) {
+        let child = this.#container.length;
+        let parent = parseInt(child / 2);
+        this.#container.push(data);
+        
+        while (parent > 0) {
+            if (this._compare(parent, child) <= 0) break;
+            
+            this._swap(parent, child);
+            
+            child = parent;
+            parent = parseInt(child / 2);
+        }
+    }
+    
+    pop() {
+        if (this.#container.length === 1) {
+            console.log("pq is empty");
+            return [undefined, undefined];
+        }
+        
+        const data = this.#container[1];
+        
+        this._swap(1, this.#container.length - 1);
+        this.#container.pop();
+        
+        let par = 1,
+            l = par * 2,
+            r = l + 1,
+            child;
+        
+        while (l < this.#container.length) {
+            if (l === this.#container.length - 1 || this._compare(l, r) <= 0) child = l;
+            else child = r;
+            
+            if (this._compare(par, child) <= 0) break;
+
+            this._swap(par, child);
+            
+            par = child;
+            l = child * 2;
+            r = l + 1;
+        }
+        
+        return data;
+    }
 }
